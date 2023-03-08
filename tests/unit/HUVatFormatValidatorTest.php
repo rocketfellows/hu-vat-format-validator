d0278b8eada712eebc9152539c3c30af32ba590a
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-03-08 20:21:00 +0400

    [CVFV-6] implemented test cases.

--- tests/unit/HUVatFormatValidatorTest.php
+++ tests/unit/HUVatFormatValidatorTest.php
@@ -30,8 +30,68 @@ class HUVatFormatValidatorTest extends TestCase
     {
         return [
             [
-                'vatNumber',
-                'isValid',
+                'vatNumber' => 'HU12345678',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'HU00000000',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'HU11111111',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'HU999999999',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '12345678',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '11111111',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '00000000',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => '99999999',
+                'isValid' => true,
+            ],
+            [
+                'vatNumber' => 'HU123456789',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'HU1234567',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '123456789',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '1234567',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => 'DE12345678',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '1',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '0',
+                'isValid' => false,
+            ],
+            [
+                'vatNumber' => '',
+                'isValid' => false,
             ],
         ];
     }
